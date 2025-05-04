678a11619ae0b8ec7f191190b500546810cf1146
Guy Wilson <wilson.guy@gmail.com>
2025-05-04 21:00:21 +0100

    Clean up

--- src/main.cpp
+++ src/main.cpp
@@ -61,10 +61,8 @@ static nrfcfg & getRadioConfig() {
     radioConfig.channel = cfg.getValueAsInteger("radio.channel");
 
     strncpy(szLocalAddress, cfg.getValue("radio.localaddress").c_str(), 31);
-    //strncpy(szRemoteAddress, cfg.getValue("radio.remoteaddress").c_str(), 31);
 
     radioConfig.localAddress = szLocalAddress;
-    // radioConfig.remoteAddress = szRemoteAddress;
     radioConfig.lnaGainOn = false;
 
     radioConfig.validate();
